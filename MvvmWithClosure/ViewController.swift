a43f5dc771af84f8b8bdef4344f776dbe58c8952
Yunus İçmen <yunus.icmen@yemeksepeti.com>
2022-03-24 13:23:26 +0300

    Initial Commit

---
+++ MvvmWithClosure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvvmWithClosure
+//
+//  Created by Yunus Icmen on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
